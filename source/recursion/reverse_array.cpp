5d18a10f238f584ca7117d3aef557e5e209af56d
Arnav Chhabra <65032719+Halleys123@users.noreply.github.com>
2024-12-27 21:39:01 +0530

    More optimizations in reverse array

--- source/recursion/reverse_array.cpp
+++ source/recursion/reverse_array.cpp
@@ -1,25 +1,36 @@
 #include "reverse_array.h"
 
-void reverse_array(int *array, int i, int j, int size)
+// void reverse_array(int *array, int i, int j, int size)
+// {
+//     // if (size % 2 == 0)
+//     // {
+//     //     if (j == i + 1)
+//     //         return;
+//     // }
+//     // else
+//     // {
+//     //     if (j == i)
+//     //         return;
+//     // }
+//     if (i > j)
+//         return;
+//     int temp = array[j];
+//     array[j] = array[i];
+//     array[i] = temp;
+// }
+
+void reverse_array(int *array, int i, int size)
 {
-    // if (size % 2 == 0)
-    // {
-    //     if (j == i + 1)
-    //         return;
-    // }
-    // else
-    // {
-    //     if (j == i)
-    //         return;
-    // }
-    if (i > j)
+    if (i > size - i - 1)
+    {
         return;
-    int temp = array[j];
-    array[j] = array[i];
+    }
+    int temp = array[size - i - 1];
+    array[size - i - 1] = array[i];
     array[i] = temp;
 }
 
 void reverse_array(int *array, int size)
 {
-    reverse_array(array, 0, size - 1, size);
+    reverse_array(array, 0, size);
 }
