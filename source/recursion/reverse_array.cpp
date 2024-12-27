2659ee365397df7a8d93f3c710e12b34628d92c3
Arnav Chhabra <65032719+Halleys123@users.noreply.github.com>
2024-12-27 21:37:02 +0530

    Refactor reverse_array function to simplify boundary checks and improve readability

--- source/recursion/reverse_array.cpp
+++ source/recursion/reverse_array.cpp
@@ -2,16 +2,18 @@
 
 void reverse_array(int *array, int i, int j, int size)
 {
-    if (size % 2 == 0)
-    {
-        if (j == i + 1)
-            return;
-    }
-    else
-    {
-        if (j == i)
-            return;
-    }
+    // if (size % 2 == 0)
+    // {
+    //     if (j == i + 1)
+    //         return;
+    // }
+    // else
+    // {
+    //     if (j == i)
+    //         return;
+    // }
+    if (i > j)
+        return;
     int temp = array[j];
     array[j] = array[i];
     array[i] = temp;
